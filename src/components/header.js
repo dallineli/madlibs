4b237ea5705afa6a6a32a432dfddb6ac84905961
Dallin Eliason <dallin@salesrabbit.com>
2019-01-23 18:20:25 -0700

    Added card and input components

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-    <div className='header'>
-        <h1>Madlibs</h1>
-        <p>Fill out the fields below and click the generate button to see the Mad Lib Story.</p>
-    </div>
- )
+        <div className='header'>
+            <h1>Madlibs</h1>
+            <p>Fill out the fields below and click the generate button to see the Mad Lib Story.</p>
+        </div>
+    )
 };
 
 export default Header;
