616c1a7d27a2ebef31711c5e0451b9e59aef2141
Dallin Eliason <dallin@salesrabbit.com>
2019-01-29 18:36:43 -0700

    added skew

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='header'>
-            <h1>Madlibs</h1>
-            <p>Fill out the fields below and click the generate button to see the Mad Lib Story.</p>
+        <div className="header">
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
         </div>
     )
-};
+}
 
 export default Header;
